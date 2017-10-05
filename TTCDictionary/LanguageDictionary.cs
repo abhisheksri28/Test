80a1da66999a61c891916b321613b9f799cff2d7
abhisheksri28 <abhishek.4.srivastava@niit-tech.com>
2017-10-05 15:57:50 +0530

    code added

---
+++ TTCDictionary/LanguageDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TTCDictionary
+{
+    public class LanguageDictionary : ILanguageDictionary
+    {
+        private Dictionary<string, string> list;
+
+        public LanguageDictionary(Dictionary<string, string> list)
+        {
+            this.list = list;
+        }
+
+        public bool Add(string language, string word)
+        {
+
+            if (list.Any(i => i.Key == language && i.Value == word))
+            {
+                return false;
+            }
+            else
+            {
+                list.Add(language, word);
+
+                return true;
+            }
+            if(!list.ContainsValue(word))
+            {
+                return false;
+            }
+        }
+
+        public bool Check(string language, string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Search(string word)
+        {
+            List<string> searchedlist = (from x in list where x.Value.Contains(word) select x.Value).ToList();
+            return searchedlist;
+        }
+    }
+}
